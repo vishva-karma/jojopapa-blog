298a7a7a2266a7be3fb6e0655921ca850c22603a
Vishva Karma <24612403+vishva-karma@users.noreply.github.com>
2026-04-19 11:52:18 +0200

    added worker files

---
+++ build.sh
@@ -0,0 +1,31 @@
+main() {
+    HUGO_VERSION="0.160.1"
+
+    export TZ=Europe/Berlin
+
+    #Install hugo
+    echo "Installing hugo v${HUGO_VERSION}"
+    curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-amd64.tar.gz
+    tar -xf hugo_extended_${HUGO_VERSION}_Linux-amd64.tar.gz
+    cp hugo /opt/buildhome
+    rm LICENSE README.md hugo_extended_${HUGO_VERSION}_Linux-amd64.tar.gz
+
+    # Verify installed versions
+    echo "Verifying dependency versions:"
+    echo "Hugo version: $(hugo version)"
+    echo "Node version: $(node --version)"
+    echo "Golang version: $(go version)"
+
+    # Clone themes repository
+    echo "Cloning themes repository"
+    git submodule update --init --recursive
+    git config core.quotePath false
+
+    # Build the site
+    echo "Building the site"
+    hugo --minify --gc
+
+}
+
+set -euo pipefail
+main "$@"
